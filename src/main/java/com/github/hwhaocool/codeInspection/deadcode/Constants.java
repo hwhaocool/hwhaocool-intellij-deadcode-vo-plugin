430cbd6c9ddc3d29f280d35766caa8baea88becc
YellowTail <yellow.tail@qq.com>
2020-12-07 14:26:27 +0800

    提交 复制于 UnusedDeclarationInspection 的代码和 插件名字之类的常量类

---
+++ src/main/java/com/github/hwhaocool/codeInspection/deadcode/Constants.java
@@ -0,0 +1,11 @@
+package com.github.hwhaocool.codeInspection.deadcode;
+
+/**
+ * @author YellowTail
+ * @since 2020-12-07
+ */
+public class Constants {
+
+    public static final String SHORT_NAME = "aayellow";
+    public static final String ALTERNATIVE_ID = "Yellow Tail";
+}
